cf92c3d56fe84ee3c7ba1c13cef611eee183dce2
MartijnBassa <49745039+MartijnBassa@users.noreply.github.com>
2022-05-14 17:28:57 +0200

    Update UserManagementApplication.java

--- src/main/java/com/kwetteruser/usermanagement/UserManagementApplication.java
+++ src/main/java/com/kwetteruser/usermanagement/UserManagementApplication.java
@@ -8,7 +8,13 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class UserManagementApplication {
 
-	public static void main(String[] args) {SpringApplication.run(UserManagementApplication.class, args);
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("../../../.env")
+				.ignoreIfMalformed()
+				.ignoreIfMissing()
+				.load();
+		SpringApplication.run(UserManagementApplication.class, args);
 	}
 
 }
